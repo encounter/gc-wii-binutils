1d9deadf3b1b932f8684e2592fe342336dceb1cc
Luke Street <luke@street.dev>
2023-12-20 15:26:00 -0700

    Add Windows builds (x86_64, arm64)

---
+++ build-windows.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -ex
+PREFIX="$(pwd)/build"
+wget -qO- https://ftp.gnu.org/gnu/binutils/binutils-2.41.tar.xz | tar -xJ
+cd binutils-2.41
+./configure --target=powerpc-unknown-eabi --prefix="$PREFIX" --disable-nls --disable-shared
+make -j$(nproc) configure-host
+make -j$(nproc)
+make install-strip
